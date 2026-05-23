90716f09fa5bf1410b46d93a574921c0753aa6f1
Lukas <99747166+LukasLow@users.noreply.github.com>
2026-05-23 15:56:31 +0200

    Reformat PdfInfo class for consistency

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,22 +1,25 @@
 export namespace src {
-  export class PdfInfo {
-    fileName: string;
-    sizeBytes: number;
-    pageCount: number;
-    dpi: number;
-    ocr: boolean;
+	
+	export class PdfInfo {
+	    fileName: string;
+	    sizeBytes: number;
+	    pageCount: number;
+	    dpi: number;
+	    ocr: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new PdfInfo(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.fileName = source["fileName"];
+	        this.sizeBytes = source["sizeBytes"];
+	        this.pageCount = source["pageCount"];
+	        this.dpi = source["dpi"];
+	        this.ocr = source["ocr"];
+	    }
+	}
 
-    static createFrom(source: any = {}) {
-      return new PdfInfo(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.fileName = source["fileName"];
-      this.sizeBytes = source["sizeBytes"];
-      this.pageCount = source["pageCount"];
-      this.dpi = source["dpi"];
-      this.ocr = source["ocr"];
-    }
-  }
 }
+
