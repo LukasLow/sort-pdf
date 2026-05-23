40611f35d2ea9da536229fa133122b1758c26221
Lukas <99747166+LukasLow@users.noreply.github.com>
2026-05-24 01:33:37 +0200

    ShowVersion → SettingsButton umbenannt (beschreibender Name)

--- frontend/src/components/sidebar/ShowSidebarFooter.svelte
+++ frontend/src/components/sidebar/ShowSidebarFooter.svelte
@@ -1,11 +1,11 @@
 <script>
     import ShowPreview from "./ShowPreview.svelte";
-    import ShowVersion from "./ShowVersion.svelte";
+    import SettingsButton from "./SettingsButton.svelte";
 
     let { onSettings } = $props();
 </script>
 
 <div class="mt-auto flex flex-col gap-2">
     <ShowPreview />
-    <ShowVersion {onSettings} />
+    <SettingsButton {onSettings} />
 </div>
